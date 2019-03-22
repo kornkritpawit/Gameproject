39ffad78522ad0eaa95fb18514a95f9676a0f214
kornkritpawit <konsoo@hotmail.com>
2019-03-22 14:46:00 +0700

    background

--- test2.py
+++ test2.py
@@ -1,1 +1,15 @@
-a=2
+import arcade
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 700
+
+class gamewindow(arcade.Window):
+    def __init__(self,width,height):
+        super().__init__(width, height)
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        arcade.start_render()
+
+if __name__ == '__main__':
+    window=gamewindow(SCREEN_WIDTH,SCREEN_HEIGHT)
+    arcade.run()

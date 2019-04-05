7ed82defa16e576b457fdf0e73da65ab95f7bafc
kornkritpawit <konsoo@hotmail.com>
2019-04-05 14:42:35 +0700

    add ball function and wood function

--- template/gam.py
+++ template/gam.py
@@ -27,20 +27,31 @@ class gamewindow(arcade.Window):
         arcade.set_background_color(arcade.color.PINK)
         self.background = arcade.load_texture('images/galaxy.jpg')
         self.wood_sprite = ModelSprite('images/wood1.png',model=self.world.wood)
+        self.ball_sprite = ModelSprite('images/ball.png',model=self.world.ball)
 
     # def update(self,delta):
 
     def on_key_press(self, key, key_modifiers):
         self.world.on_key_press(key, key_modifiers)
 
+    def on_key_release(self, key, key_modifiers):
+        self.world.on_key_release(key,key_modifiers)
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        self.world.on_mouse_press(x,y)
+
+
+
     def update(self, delta):
         self.world.update(delta)
+
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                       SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
         self.wood_sprite.draw()
+        self.ball_sprite.draw()
 
 def main():
     gamewindow()
--- template/mod.py
+++ template/mod.py
@@ -1,4 +1,5 @@
 import arcade.key
+import math
 class Model:
     def __init__(self, world, x, y, angle):
         self.world = world
@@ -12,38 +13,105 @@ class Model:
 class wood(Model):
     def __init__(self, world, x, y):
         super().__init__(world, x, y, 0)
+        self.vx = 0
+    def update(self,delta):
+        if self.x >= 875:
+            self.x = 874
+        elif self.x<=90:
+            self.x = 95
+        self.x += self.vx
+
+class ball(Model):
+    STATE_FROZEN = 1
+    STATE_STARTED = 2
+    def __init__(self, world, x, y):
+        super().__init__(world, x, y, 0)
+        self.vx = 0
+        self.vy = 0
+        self.state = ball.STATE_FROZEN
+
+    def is_hit(self, wood):
+        if self.y-20<=wood.y+25 and wood.x+125>=self.x>=wood.x-125:
+            return True
 
     def update(self,delta):
-        if self.x <= 0:
-            self.x +=50
-        elif self.x >= 1000:
-            self.x -= 50
-
+        if self.state == ball.STATE_FROZEN:
+            if self.x >= 875:
+                self.x = 874
+            elif self.x<=90:
+                self.x = 95
+        self.x += self.vx
+        self.y += self.vy
+        def bounce():
+            if self.x>=1000 or self.x<=0:
+                self.vx = -self.vx
+            if self.y>=800:
+                self.vy = -self.vy
+        bounce()
 
 class World:
-
+    STATE_FROZEN = 1
+    STATE_STARTED = 2
+    STATE_DEAD = 3
+    BALL_LIFE = 5
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.wood = wood(self, 500, 25)
+        self.ball = ball(self, 500, 65)
+        self.state = World.STATE_FROZEN
         self.score = 0
+
+    def start(self):
+        self.state = World.STATE_STARTED
+        self.ball.state = ball.STATE_STARTED
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.LEFT:
-            self.wood.x -= 50
+            self.wood.vx = -20
+            if self.state == World.STATE_FROZEN:
+                self.ball.vx = -20
         elif key == arcade.key.RIGHT:
-            self.wood.x += 50
+            self.wood.vx = 20
+            if self.state == World.STATE_FROZEN:
+                self.ball.vx = 20
+    def is_hit(self):
+        if self.ball.y-20<=self.wood.y+25 and self.wood.x+125>=self.ball.x>=self.wood.x-125:
+            return True
+    def reset(self):
+        if self.ball.y < 0:
+            self.wood = wood(self, 500, 25)
+            self.ball = ball(self, 500, 65)
+            self.state = World.STATE_FROZEN
+
+
+    def on_mouse_press(self,x,y):
+        dx = (x - self.ball.x)
+        dy = (y - self.ball.y)
+        angle = math.atan2(dy,dx)
+        vx = math.cos(angle)
+        vy = math.sin(angle)
+        if self.state == World.STATE_FROZEN:
+            self.ball.state = ball.STATE_STARTED
+            self.ball.vx = vx*10
+            self.ball.vy = vy*10
+            self.state = World.STATE_STARTED
+
+    def on_key_release(self, key, key_modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.wood.vx = 0
+        if self.ball.state == self.STATE_FROZEN:
+            if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+                self.ball.vx = 0
 
     def update(self, delta):
         self.wood.update(delta)
+        self.ball.update(delta)
+        if self.state == self.STATE_STARTED:
+            if self.is_hit():
+                # self.ball.vx = -self.ball.vx
+                self.ball.vy = -self.ball.vy
+        self.reset()
 
-    # def update(self, delta):
-    #     self.ship.update(delta)
-    #     if self.ship.hit(self.gold, 10):
-    #         self.gold.random_location()
-    #         self.score += 1
-    #
-    # def on_key_press(self, key, key_modifiers):
-    #     if key == arcade.key.SPACE:
-    #         self.ship.switch_direction()
+
 

22f5a59149d2b6cd7bee47ca26139f72f19f0287
kornkritpawit <konsoo@hotmail.com>
2019-05-10 14:07:05 +0700

    create score, ball life, and restart

---
+++ template/gam.py
@@ -0,0 +1,116 @@
+import arcade
+from template.mod import World
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+BLOCK_HEIGHT = 60
+BLOCK_WIDTH = 100
+text = 'You need to break all bricks to win this game.'
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+            self.angle = self.model.angle
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
+
+
+class gamewindow(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, 'Bouncing ball')
+        self.world = World(SCREEN_WIDTH,SCREEN_HEIGHT)
+        print(self.world.brick.has_dot)
+        arcade.set_background_color(arcade.color.PINK)
+        self.background = arcade.load_texture('images/background.jpg')
+        self.wood_sprite = ModelSprite('images/wood1.png',model=self.world.wood)
+        self.ball_sprite = ModelSprite('images/ball.png',model=self.world.ball)
+        self.brick_drawer = BrickDrawer(self.world.brick)
+
+    # def update(self,delta):
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
+
+    def on_key_release(self, key, key_modifiers):
+        self.world.on_key_release(key,key_modifiers)
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        self.world.on_mouse_press(x,y)
+
+
+
+    def update(self, delta):
+        self.world.update(delta)
+
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        if self.world.state == self.world.STATE_FROZEN:
+            arcade.draw_text(f'{"You need to break all bricks to win this game.":^100}'+'\n'
+                             +f'{"If the ball falls under the wood,your life will decrease.":^100}'+'\n'
+                            +f'{"Press left or right arrow key to move the wood to bounce th ball":^100}'+
+                             '\n'+f'{"Press somewhere on the screen to shoot the ball and to start the game:":^100}'
+                              +'\n'+f' {"Good luck! Beware of your life:":^100}',0
+                             ,SCREEN_HEIGHT//2 - 200,arcade.color.WHITE_SMOKE,20)
+        if self.world.state == self.world.STATE_WIN:
+            arcade.draw_text(f'{"You win the game!!":^100}' + '\n'
+                             + f'{"Press space to restart or enter to exit":^100}'
+                             , 0, SCREEN_HEIGHT // 2, arcade.color.HEART_GOLD, 25)
+        elif self.world.ball_life>=0:
+            self.brick_drawer.draw()
+            self.wood_sprite.draw()
+            self.ball_sprite.draw()
+        else:
+            arcade.draw_text(f'{"You lose":^100}'+'\n'+f'{"Press space to restart or enter to exit":^100}'
+                             ,0,SCREEN_HEIGHT//2,arcade.color.RED_DEVIL,25)
+
+        arcade.draw_text('BALL LIFE: '+str(self.world.ball_life),
+                         self.width - 200, self.height - 50,
+                         arcade.color.YELLOW, 20)
+        arcade.draw_text('Your score: '+str(self.world.score),0,self.height - 50,
+                         arcade.color.WHITE_SMOKE,20)
+class BrickDrawer:
+    def __init__(self, brick):
+        self.brick = brick
+        self.width = self.brick.width
+        self.height = self.brick.height
+
+        self.brick_sprite = arcade.Sprite('images/wall.png')
+
+    def get_sprite_position(self, r, c):
+        x = c * BLOCK_WIDTH + (BLOCK_WIDTH // 2);
+        y = r * BLOCK_HEIGHT + (BLOCK_HEIGHT + (BLOCK_HEIGHT // 2))+360
+        return x,y
+    def draw_sprite(self, sprite, r, c):
+        x, y = self.get_sprite_position(r, c)
+        sprite.set_position(x, y)
+        sprite.draw()
+        return x,y
+
+    def draw(self):
+        pos= []
+        for r in range(self.height):
+            for c in range(self.width):
+                if self.brick.has_dot_at(r, c):
+                    self.draw_sprite(self.brick_sprite, r, c)
+
+
+
+
+def main():
+    window = gamewindow()
+    arcade.set_window(window)
+    arcade.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ template/mod.py
@@ -0,0 +1,216 @@
+import arcade.key
+import math
+from template.pos import check_brick_y
+class Model:
+    def __init__(self, world, x, y, angle):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.angle = 0
+
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <=hit_size)
+
+class wood(Model):
+    def __init__(self, world, x, y):
+        super().__init__(world, x, y, 0)
+        self.vx = 0
+    def update(self,delta):
+        if self.x >= 875:
+            self.x = 874
+        elif self.x<=90:
+            self.x = 95
+        self.x += self.vx
+
+
+class ball(Model):
+    STATE_FROZEN = 1
+    STATE_STARTED = 2
+
+    def __init__(self, world, x, y,brick):
+        super().__init__(world, x, y, 0)
+        self.vx = 0
+        self.vy = 0
+        self.state = ball.STATE_FROZEN
+        self.brick = brick
+
+
+    def is_hit(self, wood):
+        if self.y-20<=wood.y+25 and wood.x+125>=self.x>=wood.x-125:
+            return True
+    def brick_bounce_y(self):
+        y,x = check_brick_y(self.x,self.y)
+
+        if y==-1 or x == -1:
+            return
+        if self.brick.has_dot[y][x] == True:
+            self.brick.has_dot[y][x] = False
+            print(self.brick.has_dot)
+            return True
+        # elif self.brick.has_dot[y][x] == False:
+        #     if self.brick.has_dot[y+1][x]==True:
+        #         self.brick_bounce_x()
+    # def brick_bounce_x(self):
+    #     y,x,v = check_brick_x(self.x,self.y,self.vx)
+    #     if self.brick.has_dot[y,x]:
+    #         self.brick.has_dot[y,x]=False
+    #         if v == 'n':
+    #             self.vx=-self.vx
+    #         else:
+    #             self.vx=-self.vx
+
+
+
+
+    def update(self,delta):
+        if self.state == ball.STATE_FROZEN:
+            if self.x >= 875:
+                self.x = 874
+            elif self.x<=90:
+                self.x = 95
+        self.x += self.vx
+        self.y += self.vy
+        def bounce():
+            if self.x>=1000 or self.x<=0:
+                self.vx = -self.vx
+            if self.y>=800:
+                self.vy = -self.vy
+        bounce()
+        if self.brick_bounce_y():
+            self.vy = -self.vy
+            self.world.score+=100
+        # else:
+        #     self.vx = -self.vx
+class Brick:
+    def __init__(self, world):
+        self.map = [ '##########',
+                     '##########',
+                     '##########',
+                     '##########',
+                     '##########',
+                    ]
+        self.height = len(self.map)
+        self.width = len(self.map[0])
+        self.init_dot_data()
+
+
+
+    def init_dot_data(self):
+        has_dot = {}
+        for r in range(self.height):
+            has_dot[r] = {}
+            for c in range(self.width):
+                has_dot[r][c] = self.map[r][c] == '#'
+        self.has_dot = has_dot
+
+    def init_brick_pos(self,pos):
+        self.pos = pos
+        if self.n<=0:
+            print(self.pos)
+            self.n+=1
+
+    def remove_dot_at(self, r, c):
+        self.has_dot[r][c] = False
+
+    # def has_wall_at(self, r, c):
+    #     return self.map[r][c] == '#'
+
+    def has_dot_at(self, r, c):
+        return self.has_dot[r][c]
+
+class World:
+    STATE_FROZEN = 1
+    STATE_STARTED = 2
+    STATE_DEAD = 3
+    STATE_WIN = 4
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.wood = wood(self, 500, 25)
+        self.state = World.STATE_FROZEN
+        self.brick = Brick(self)
+        self.ball = ball(self, 500, 65,self.brick)
+
+        self.score = 0
+        self.ball_life = 5
+
+    def start(self):
+        self.state = World.STATE_STARTED
+        self.ball.state = ball.STATE_STARTED
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.LEFT:
+            self.wood.vx = -20
+            if self.state == World.STATE_FROZEN:
+                self.ball.vx = -20
+        elif key == arcade.key.RIGHT:
+            self.wood.vx = 20
+            if self.state == World.STATE_FROZEN:
+                self.ball.vx = 20
+        if self.state == self.STATE_DEAD or self.state == self.STATE_WIN:
+            if key == arcade.key.SPACE:
+                self.state =self.STATE_FROZEN
+                self.reset_game()
+            elif key == arcade.key.ENTER:
+                quit()
+    def is_hit(self):
+        if self.ball.y-20<=self.wood.y+25 and self.wood.x+125>=self.ball.x>=self.wood.x-125:
+            return True
+
+    def reset(self):
+        if self.ball.y < 0:
+            self.wood.x = 500
+            self.ball.x = self.wood.x
+            self.ball.y = 65
+            self.ball.state = ball.STATE_FROZEN
+            self.state = World.STATE_FROZEN
+            self.ball.vx = 0
+            self.ball.vy = 0
+            self.ball_life -= 1
+            # self.ball.state =
+    def reset_game(self):
+        self.score=0
+        self.ball_life=5
+        del(self.brick.has_dot)
+        self.brick.init_dot_data()
+
+
+    def on_mouse_press(self,x,y):
+        dx = (x - self.ball.x)
+        dy = (y - self.ball.y)
+        angle = math.atan2(dy,dx)
+        vx = math.cos(angle)
+        vy = math.sin(angle)
+        if self.state == World.STATE_FROZEN:
+            self.ball.state = ball.STATE_STARTED
+            self.ball.vx = vx*10
+            self.ball.vy = vy*10
+            self.state = World.STATE_STARTED
+
+    def on_key_release(self, key, key_modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.wood.vx = 0
+        if self.ball.state == self.STATE_FROZEN:
+            if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+                self.ball.vx = 0
+
+    def update(self, delta):
+        self.wood.update(delta)
+        self.ball.update(delta)
+        if self.state == self.STATE_STARTED:
+            if self.is_hit():
+                if self.wood.vx == 0:
+                    self.ball.vy = -self.ball.vy
+                elif self.wood.vx!=0:
+                    self.ball.vy = -self.ball.vy
+                    self.ball.vx = -self.ball.vx
+        self.reset()
+        if self.ball_life<0:
+            self.state=self.STATE_DEAD
+        if self.score == 5000:
+            self.state = self.STATE_WIN
+
+
+
+
+
